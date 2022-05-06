264217383b99175cf8abc0bf479ac172e6a96064
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-05-05 18:29:22 -0700

    Update bestbuy_deals.go

--- bestbuy/bestbuy_deals.go
+++ bestbuy/bestbuy_deals.go
@@ -3,7 +3,10 @@ package bestbuy
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
+	"sync"
 )
 
 // Open Box identifers:
@@ -25,11 +28,12 @@ type OpenBoxItem struct {
 	Fair               OpenBoxItemInfo
 }
 
-type DealInfo struct {
+type Deal struct {
 	Name     string
 	Price    float64
 	Brand    string
 	Link     string
+	Discount int
 	CartLink string
 }
 
@@ -107,15 +111,15 @@ func fetchOpenBoxOffers(skuId string) (OpenBoxItem, error) {
 	return openBoxItem, nil
 }
 
-func sendToDiscord(deal DealInfo) error {
+func sendToDiscord(deal Deal) error {
 	return nil
 }
 
-func sendToTwitter(deal DealInfo) error {
+func sendToTwitter(deal Deal) error {
 	return nil
 }
 
-func sendToTelegram(deal DealInfo) error {
+func sendToTelegram(deal Deal) error {
 	return nil
 }
 
@@ -150,7 +154,192 @@ func GetLatestDealOffers() {
 
 }
 
-// returns the latest deals
-func FetchLatestDeals(searchQuery string, minimumDiscountPercent int) {
+func GetDealInfo(client *http.Client, skuId string) (Deal, error) {
+	url := "https://www.bestbuy.com/api/3.0/priceBlocks?skus=" + skuId
+	method := "GET"
 
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return Deal{}, err
+	}
+
+	req.Header.Add("authority", "www.bestbuy.com")
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("accept-language", "en-US,en;q=0.9")
+	req.Header.Add("referer", "https://www.bestbuy.com")
+	req.Header.Add("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Microsoft Edge\";v=\"101\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Add("sec-fetch-dest", "empty")
+	req.Header.Add("sec-fetch-mode", "cors")
+	req.Header.Add("sec-fetch-site", "same-origin")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return Deal{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Deal{}, err
+	}
+
+	dealInfoResp := DealInfoResponse{}
+
+	json.Unmarshal(body, &dealInfoResp)
+
+	return Deal{Name: dealInfoResp[0].Sku.Names.Short, Price: dealInfoResp[0].Sku.Price.CurrentPrice, Brand: dealInfoResp[0].Sku.Brand.Brand, Link: "https://www.bestbuy.com" + dealInfoResp[0].Sku.URL, Discount: 0, CartLink: "https://api.bestbuy.com/click/-/" + skuId + "/cart"}, nil
+}
+
+func GetItemDiscount(client *http.Client, skuId string) (int, error) {
+	url := "https://www.bestbuy.com/priceview/query/sku?includes=price,holidayPriceDealMessages&context=shop-holiday&layout=small&viewType=price&skuId=" + skuId
+	method := "GET"
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header.Add("authority", "www.bestbuy.com")
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("accept-language", "en-US,en;q=0.9")
+	req.Header.Add("referer", "https://www.bestbuy.com")
+	req.Header.Add("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Microsoft Edge\";v=\"101\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Add("sec-fetch-dest", "empty")
+	req.Header.Add("sec-fetch-mode", "cors")
+	req.Header.Add("sec-fetch-site", "same-origin")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	itemDiscountResp := ItemDiscountResponse{}
+
+	json.Unmarshal(body, &itemDiscountResp)
+	return itemDiscountResp.TotalSavingsPercent, nil
+}
+
+func skuExists(skuList []string, sku string) bool {
+	for _, v := range skuList {
+		if v == sku {
+			return true
+		}
+	}
+	return false
+}
+
+func getQueryRecommendations(client *http.Client, searchQuery string) ([]string, error) {
+	searchQuery = url.QueryEscape(searchQuery)
+
+	url := "https://www.bestbuy.com/suggest/v1/fragment/suggest/www?query=" + searchQuery
+	method := "GET"
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("authority", "www.bestbuy.com")
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("accept-language", "en-US,en;q=0.9")
+	req.Header.Add("referer", "https://www.bestbuy.com")
+	req.Header.Add("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Microsoft Edge\";v=\"101\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Add("sec-fetch-dest", "empty")
+	req.Header.Add("sec-fetch-mode", "cors")
+	req.Header.Add("sec-fetch-site", "same-origin")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	queryRecResp := QueryRecResponse{}
+
+	json.Unmarshal(body, &queryRecResp)
+
+	var skuList []string
+
+	for _, suggestions := range queryRecResp.SuggestionResponse.Suggestions {
+		for _, products := range suggestions.Products {
+			if skuExists(skuList, products.SkuID) == false {
+				skuList = append(skuList, products.SkuID)
+			}
+		}
+	}
+
+	return skuList, nil
+}
+
+// returns the latest deals under a particular search query
+func FetchLatestDeals(searchQuery string, minimumDiscountPercent int) ([]Deal, error) {
+	client := &http.Client{}
+	var wg sync.WaitGroup
+
+	skus, err := getQueryRecommendations(client, searchQuery)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	var dealList []Deal
+
+	for _, sku := range skus {
+
+		go func(skuId string) {
+			wg.Add(1)
+
+			var itemDiscount int
+			var deal Deal
+
+			itemDiscount, err = GetItemDiscount(client, skuId)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			if itemDiscount >= minimumDiscountPercent {
+				// the request made in GetDealInfo also contains the total savings percent
+				// but GetItemDiscount may account for further (hidden?) discounts during holidays
+				deal, err = GetDealInfo(client, skuId)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+
+				deal.Discount = itemDiscount
+
+				dealList = append(dealList, deal)
+			}
+			wg.Done()
+		}(sku)
+	}
+
+	wg.Wait()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dealList, nil
 }
