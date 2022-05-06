301ddee966ab5a801bd91affbec1f9ed8d7b0760
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-05-05 18:42:39 -0700

    Update bestbuy_types.go

--- bestbuy/bestbuy_types.go
+++ bestbuy/bestbuy_types.go
@@ -59,3 +59,173 @@ type OpenBoxResponse struct {
 	TotalSavings        int64   `json:"totalSavings"`
 	TotalSavingsPercent int64   `json:"totalSavingsPercent"`
 }
+
+type ItemDiscountResponse struct {
+	ShowBuyNowPayLater              bool          `json:"showBuyNowPayLater"`
+	PaymentOptions                  []interface{} `json:"paymentOptions"`
+	ShowCustomerPrice               bool          `json:"showCustomerPrice"`
+	CustomerPrice                   float64       `json:"customerPrice"`
+	ShowFavorFree                   bool          `json:"showFavorFree"`
+	ShowTotalTechInfo               bool          `json:"showTotalTechInfo"`
+	ShowLivelyPricingInfo           bool          `json:"showLivelyPricingInfo"`
+	ShowPrefix                      bool          `json:"showPrefix"`
+	ShowPerMonth                    bool          `json:"showPerMonth"`
+	ShowPriceDisclaimer             bool          `json:"showPriceDisclaimer"`
+	ShowSaleMessage                 bool          `json:"showSaleMessage"`
+	AbbreviatedRegularPrice         string        `json:"abbreviatedRegularPrice"`
+	Description                     string        `json:"description"`
+	IsAlone                         bool          `json:"isAlone"`
+	ShowSavingsRegularPrice         bool          `json:"showSavingsRegularPrice"`
+	MessagePrefix                   string        `json:"messagePrefix"`
+	MessageSuffix                   string        `json:"messageSuffix"`
+	MessagePrefixWithSavingsPercent string        `json:"messagePrefixWithSavingsPercent"`
+	Total                           int           `json:"total"`
+	ShowFinanceOption               bool          `json:"showFinanceOption"`
+	TotalSavingsPercent             int           `json:"totalSavingsPercent"`
+	ShowPercentOff                  bool          `json:"showPercentOff"`
+	DisableSavingsPercent           bool          `json:"disableSavingsPercent"`
+	HasMobileContracts              bool          `json:"hasMobileContracts"`
+	DisableGreenSavingsBrick        bool          `json:"disableGreenSavingsBrick"`
+	ShowContextualPriceMessaging    bool          `json:"showContextualPriceMessaging"`
+	DealExpirationTimeStamp         interface{}   `json:"dealExpirationTimeStamp"`
+	DisableExpirationTimer          bool          `json:"disableExpirationTimer"`
+	ExpirationDateThreshold         int           `json:"expirationDateThreshold"`
+	CountDownThreshold              int           `json:"countDownThreshold"`
+	ShowBundleAndSave               bool          `json:"showBundleAndSave"`
+	Href                            string        `json:"href"`
+	DataTrack                       string        `json:"dataTrack"`
+	ShowMyBByPrice                  bool          `json:"showMyBByPrice"`
+	GwpEmbedClientSideOnly          bool          `json:"gwpEmbedClientSideOnly"`
+	Layout                          string        `json:"layout"`
+	SkuID                           string        `json:"skuId"`
+	MmtPricing                      bool          `json:"mmtPricing"`
+	ShowLink                        bool          `json:"showLink"`
+	ContextParam                    string        `json:"contextParam"`
+	ShowPriceBadge                  bool          `json:"showPriceBadge"`
+	ShowPreviousPrice               bool          `json:"showPreviousPrice"`
+	ShowPaidMemberBadge             bool          `json:"showPaidMemberBadge"`
+	ShowPMGBadge                    bool          `json:"showPMGBadge"`
+	PreferredBadgingStr             string        `json:"preferredBadgingStr"`
+	ShowMyBBYOffer                  bool          `json:"showMyBBYOffer"`
+	ShowHolidayMessageBadge         bool          `json:"showHolidayMessageBadge"`
+	OverridePaidMemberBadgeHTML     string        `json:"overridePaidMemberBadgeHTML"`
+	ShowOffers                      bool          `json:"showOffers"`
+	IsMobileContractSelected        bool          `json:"isMobileContractSelected"`
+	ShowSuco                        bool          `json:"showSuco"`
+	ShowSmallPrice                  bool          `json:"showSmallPrice"`
+	ShowTotalCostClarity            bool          `json:"showTotalCostClarity"`
+	ShowBlackFridayHolidayMessage   bool          `json:"showBlackFridayHolidayMessage"`
+	ShowLowestPriceOfTheSeason      bool          `json:"showLowestPriceOfTheSeason"`
+	PriceChangeTotalSavingsAmount   int           `json:"priceChangeTotalSavingsAmount"`
+	PriceChangeTotalSavingsPercent  int           `json:"priceChangeTotalSavingsPercent"`
+}
+
+type QueryRecResponse struct {
+	SuggestionResponse struct {
+		Count             int `json:"count"`
+		QueryResponseTime int `json:"queryResponseTime"`
+		Track             struct {
+			Location struct {
+				Applied   string `json:"applied"`
+				Requested string `json:"requested"`
+			} `json:"location"`
+			Variant string `json:"variant"`
+		} `json:"track"`
+		SpellCheck struct {
+			CorrectlySpelled bool   `json:"correctlySpelled"`
+			CorrectedQuery   string `json:"correctedQuery"`
+			OriginalQuery    string `json:"originalQuery"`
+		} `json:"spellCheck"`
+		Suggestions []struct {
+			Products []struct {
+				SkuID  string `json:"skuId"`
+				Source string `json:"source"`
+				Type   string `json:"type"`
+			} `json:"products"`
+			Category []struct {
+				ID   string `json:"id"`
+				Name string `json:"name"`
+			} `json:"category,omitempty"`
+			Term string `json:"term"`
+		} `json:"suggestions"`
+	} `json:"suggestionResponse"`
+}
+
+type DealInfoResponse []struct {
+	Sku struct {
+		Attributes struct {
+			LowPriceGuaranteedProduct bool `json:"lowPriceGuaranteedProduct"`
+		} `json:"attributes"`
+		Brand struct {
+			Brand string `json:"brand"`
+		} `json:"brand"`
+		ButtonState struct {
+			Purchasable bool   `json:"purchasable"`
+			ButtonState string `json:"buttonState"`
+			DisplayText string `json:"displayText"`
+			SkuID       string `json:"skuId"`
+		} `json:"buttonState"`
+		Condition string `json:"condition"`
+		Names     struct {
+			Short string `json:"short"`
+		} `json:"names"`
+		Price struct {
+			CurrentPrice       float64 `json:"currentPrice"`
+			PricingType        string  `json:"pricingType"`
+			SmartPricerEnabled bool    `json:"smartPricerEnabled"`
+			PriceDomain        struct {
+				SkuID               string  `json:"skuId"`
+				PriceEventType      string  `json:"priceEventType"`
+				RegularPrice        float64 `json:"regularPrice"`
+				CurrentPrice        float64 `json:"currentPrice"`
+				CustomerPrice       float64 `json:"customerPrice"`
+				TotalSavings        float64 `json:"totalSavings"`
+				TotalSavingsPercent float64 `json:"totalSavingsPercent"`
+				IsMAP               bool    `json:"isMAP"`
+				OfferQualifiers     []struct {
+					OfferID      string `json:"offerId"`
+					OfferName    string `json:"offerName"`
+					OfferVersion int    `json:"offerVersion"`
+					ID           int    `json:"id"`
+				} `json:"offerQualifiers"`
+				GiftSkus []struct {
+					SkuID    string `json:"skuId"`
+					Quantity int    `json:"quantity"`
+					OfferID  string `json:"offerId"`
+				} `json:"giftSkus"`
+				CurrentAsOfDate string `json:"currentAsOfDate"`
+			} `json:"priceDomain"`
+		} `json:"price"`
+		ProductType string `json:"productType"`
+		Properties  struct {
+			ServicePlansType      string `json:"servicePlansType"`
+			ServicePlanDetailsURL string `json:"servicePlanDetailsURL"`
+		} `json:"properties"`
+		GspAppleCare []struct {
+			SkuID string `json:"skuId"`
+			Name  string `json:"name"`
+			Price struct {
+				CurrentPrice float64 `json:"currentPrice"`
+			} `json:"price"`
+			Type           string `json:"type"`
+			ProtectionType string `json:"protectionType"`
+			Term           string `json:"term"`
+			PaymentType    string `json:"paymentType"`
+		} `json:"gspAppleCare"`
+		SkuID string `json:"skuId"`
+		URL   string `json:"url"`
+		Class struct {
+			DisplayName string `json:"displayName"`
+			ID          string `json:"id"`
+		} `json:"class"`
+		Department struct {
+			DisplayName string `json:"displayName"`
+			ID          string `json:"id"`
+		} `json:"department"`
+		Subclass struct {
+			DisplayName string `json:"displayName"`
+			ID          string `json:"id"`
+		} `json:"subclass"`
+		InkSubscriptions []interface{} `json:"inkSubscriptions"`
+	} `json:"sku"`
+}
